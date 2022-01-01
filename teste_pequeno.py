5c90d4700629d224283c11cd693748d6bb74b855
andreazza <carlos.andreazza@gmail.com>
2022-01-01 15:48:39 -0300

    testando

--- teste_pequeno.py
+++ teste_pequeno.py
@@ -5,12 +5,4 @@ Created on Sun Dec  8 15:42:16 2019
 @author: carlo
 """
 
-import numpy as np
-
-climate_data = np.array([[73, 67, 43],
-                         [91, 88, 64],
-                         [87, 134, 58],
-                         [102, 43, 37],
-                         [69, 96, 70]])
-
-weights = np.array([0.3, 0.2, 0.5])
+teste
